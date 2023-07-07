166db913efd62af31673590c17594672ef40d5a3
Krzysztof Cybulski <k.cybulski.dev@tuta.io>
2023-07-07 15:34:12 +0200

    Some rudimentary code to check the Taylor method

---
+++ demo.py
@@ -0,0 +1,81 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+from matplotlib import pyplot as plt
+
+plot = False
+
+# Part 1: Simulate some data
+# generate data with a simple model that we can invert with DEM
+
+## Define the model
+# x' = f(x) + w
+# y  = g(x) + z
+
+## Simplest case
+# x' = Ax
+# y  = Ix + z
+# for white noise z
+
+x0 = np.array([0, 1])
+A = np.array([[0, 1], [-1, 0]])
+
+noise_sd = 0.1
+
+def f(t, x):
+    return A @ x
+
+# Simulate the data
+t_start = 0
+t_end = 20
+t_span = (t_start, t_end)
+dt = 0.1
+ts = np.arange(start=t_start, stop=t_end, step=dt)
+out = solve_ivp(f, t_span, x0, t_eval=ts)
+
+xs = out.y
+ys = np.random.normal(xs, noise_sd)
+
+if plot:
+    plt.plot(ts, ys[0, :])
+    plt.plot(ts, ys[1, :])
+    plt.show()
+
+
+# Part 2: Invert the model
+
+## Extract generalized y, i.e. y_tilde
+# we will use the inverted Taylor trick, using 6 derivatives
+
+# say we focus on the 30th timestep
+p = 6
+t_step = 30
+
+
+# x0 = np.array([[0], [1]])
+# A = np.array([[0, 1], [-1, 0]])
+
+# def evolve_system(x_0, A, iter=100):
+#     # Evolves a system by Euler method
+#     xs = x_0
+#     x_i = x_0
+#     for i in range(1, iter):
+#         x_i = A @ x_i
+#         xs = np.hstack([xs, x_i])
+#     return xs
+
+# xs = evolve_system(x_0, A)
+
+def sin_gen(n, t):
+    matr = np.zeros((1, n))
+    for i in range(n):
+        i_mod = np.mod(i, 4)
+        match i_mod:
+            case 0:
+                matr[0, i] = np.sin(t)
+            case 1:
+                matr[0, i] = np.cos(t)
+            case 2:
+                matr[0, i] = -np.sin(t)
+            case 3:
+                matr[0, i] = -np.cos(t)
+    return matr
---
+++ test_core.py
@@ -0,0 +1,92 @@
+import numpy as np
+
+from hdm.core import taylor_mat
+
+
+# Sanity check: Can we convert from generalized coordinates to normal coordinates?
+def sin_gen(n, t):
+    matr = np.zeros((n, 1))
+    for i in range(n):
+        i_mod = np.mod(i, 4)
+        match i_mod:
+            case 0:
+                matr[i, 0] = np.sin(t)
+            case 1:
+                matr[i, 0] = np.cos(t)
+            case 2:
+                matr[i, 0] = -np.sin(t)
+            case 3:
+                matr[i, 0] = -np.cos(t)
+    return matr
+
+def cos_gen(n, t):
+    sin_gen_ = sin_gen(n + 1, t)
+    return sin_gen_[1:]
+
+def combine_gen(gen1, gen2):
+    # https://stackoverflow.com/a/5347492
+    out = np.empty((gen1.shape[0] + gen2.shape[0], 1), dtype=gen1.dtype)
+    out[0::2] = gen1
+    out[1::2] = gen2
+    return out
+
+
+def plot_taylor_approx_for_sin():
+    """
+    Sanity check to see if the taylor_mat method works for a single variable function
+    """
+    t_start = 0
+    t_end = 20
+    t_span = (t_start, t_end)
+    dt = 0.1
+    ts = np.arange(start=t_start, stop=t_end, step=dt)
+    xs = np.sin(ts)
+
+    ts_to_check = [5, 10, 15]
+    p = 20
+
+    plt.plot(ts, xs, color='red')
+
+    for t in ts_to_check:
+        x_gen = sin_gen(p + 1, t)
+        x_appr = taylor_mat(p, dt) @ x_gen
+        ts_for_appr = t - np.ceil(p/2) * dt + ((np.arange(p + 1)) * dt)
+        plt.plot(ts_for_appr, x_appr, color='green')
+
+    plt.show()
+
+def plot_taylor_approx_for_sin_cos():
+    """
+    Sanity check to see if the taylor_mat method works for a two variable function
+    """
+    t_start = 0
+    t_end = 20
+    t_span = (t_start, t_end)
+    dt = 0.1
+    ts = np.arange(start=t_start, stop=t_end, step=dt)
+    x1s = np.sin(ts)
+    x2s = np.cos(ts)
+
+    ts_to_check = [5, 10, 15]
+    p = 20
+
+    plt.plot(ts, x1s, color='red')
+    plt.plot(ts, x2s, color='pink')
+
+    for t in ts_to_check:
+        x1_gen = sin_gen(p + 1, t)
+        x2_gen = cos_gen(p + 1, t)
+        x_gen = combine_gen(x1_gen, x2_gen)
+        t_mat = np.kron(taylor_mat(p, dt), np.eye(2))
+        x_appr = t_mat @ x_gen
+        x1_appr = x_appr[0::2]
+        x2_appr = x_appr[1::2]
+        ts_for_appr = t - np.ceil(p/2) * dt + ((np.arange(p + 1)) * dt)
+        plt.plot(ts_for_appr, x1_appr, color='green')
+        plt.plot(ts_for_appr, x2_appr, color='blue')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    plot_taylor_approx_for_sin()
