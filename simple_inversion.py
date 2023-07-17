89e0bb3ac7f76692051d5db3bf4aca11991b54f6
Krzysztof Cybulski <k.cybulski.dev@tuta.io>
2023-07-17 14:06:14 +0200

    Some skeleton code for LTI inversion example

--- simple_inversion.py
+++ simple_inversion.py
@@ -7,6 +7,7 @@ import numpy as np
 from scipy.signal import lti, cont2discrete, lsim
 
 from hdm.noise import generate_noise_conv
+from hdm.core import taylor_mat
 
 # Simulating a system
 # we will consider a simple, single variable model defined by
@@ -20,30 +21,67 @@ def simple_lti(alpha, beta):
     D = np.array([[0, 0]]) # 0 for noise
     return lti(A, B, C, D)
 
-# Properties of the system
-alpha = -0.5
-beta = 5
+
+class DEMModelLTI:
+    # TODO: This is unfinished, and possibly not even a good strategy for implementing it
+
+    # All of the priors to be kept
+    # following notation of Anil Meera and Wisse
+
+    # Dynamically varying parameters
+    x_mu # states
+    v_mu # inputs
+    x_cov
+    v_cov
+
+    # Parameters constant throughout simulation
+    A_mu
+    B_mu
+    C_mu
+
+    A_cov
+    B_cov
+    C_cov
+
+    lam
+
+    def initialize(A_init, B_init, C_init):
+        self.A_mu = A_init
+        self.B_mu = B_init
+        self.C_mu = C_init
 
 
-sys = simple_lti(alpha, beta)
-# Properties of noise
-var = 2.5
-sig = 1
+    def free_action():
+        # FIXME
+        pass
 
 
-# Properties of the simulation
-dt = 0.1
-n = 500
-t = np.arange(n) * dt
-u = np.zeros(n)
-u[100:150] = 1
-x0 = np.array([100])
+if __name__ == '__main__':
+    # Properties of the system
+    alpha = -0.5
+    beta = 1
 
-# Run the simulation and generate noise
-noise = generate_noise_conv(n, dt, var, sig)
-u_with_noise = np.stack([u, noise], axis=1)
-_, yout, xout = lsim(sys, u_with_noise, t, x0, interp=False)
-plt.plot(t, yout, label="System output")
-plt.plot(t, noise, label="Noise")
-plt.legend()
-plt.show()
+
+    sys = simple_lti(alpha, beta)
+    # Properties of noise
+    var = 2.5
+    sig = 1
+
+
+    # Properties of the simulation
+    dt = 0.1
+    n = 500
+    t = np.arange(n) * dt
+    u = np.zeros(n)
+    u[100:150] = 5
+    x0 = np.array([100])
+
+    # Run the simulation and generate noise
+    noise = generate_noise_conv(n, dt, var, sig)
+    u_with_noise = np.stack([u, noise], axis=1)
+    _, yout, xout = lsim(sys, u_with_noise, t, x0, interp=False)
+    plt.plot(t, u, label="System input")
+    plt.plot(t, yout, label="System output")
+    plt.plot(t, noise, label="Noise")
+    plt.legend()
+    plt.show()
