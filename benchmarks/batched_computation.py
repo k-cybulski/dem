d71edfc79229af5d7d9fad71ca97d308724fb71d
Krzysztof Cybulski <k.cybulski.dev@tuta.io>
2023-08-12 15:22:18 +0200

    Make directory for implementation benchmarks

--- benchmarks/batched_computation.py
+++ benchmarks/batched_computation.py
@@ -1,6 +1,5 @@
 """
-This little script is used to evaluate whether or not it is better to vectorize
-computations over the timesteps, or to do them in a loop.
+This little script is used to compare implementations of various functions used in computation of free action.
 """
 
 import numpy as np
@@ -11,7 +10,7 @@ from tabulate import tabulate
 from matplotlib import pyplot as plt
 
 from hdm.dummy import sin_gen, cos_gen, combine_gen
-from hdm.dem.naive import generalized_func, _fix_grad_shape
+from hdm.dem.naive import _fix_grad_shape
 # from hdm.dem.batched import generalized_func as generalized_func_batched
 
 def _fix_grad_shape_batch(tensor):
@@ -26,6 +25,47 @@ def _fix_grad_shape_batch(tensor):
         return tensor[batch_selection,:,0,batch_selection,:,0]
     else:
         raise ValueError(f"Unexpected hessian shape: {tuple(tensor.shape)}")
+
+def generalized_func_naive(func, mu_x_tilde, mu_v_tilde, m_x, m_v, p, params):
+    """
+    Computes generalized application of a function, assuming local linearity.
+    Used to find g_tilde and f_tilde.
+
+    Args:
+        func (function): function to apply (g or f)
+        mu_x_tilde (Tensor): generalized vector of estimated state means
+        mu_v_tilde (Tensor): generalized vector of estimated input means
+        m_x (int): number of elements in state vector
+        m_v (int): number of elements in input vector
+        p (int): numer of derivatives in generalized vectors
+        params (Tensor): parameters of func
+    """
+    # TODO: Ensure correctness of shapes, e.g. shape (2, 1) instead of (2,). This causes lots of errors down the road.
+    assert mu_x_tilde.shape == (m_x * (p + 1), 1)
+    assert mu_v_tilde.shape == (m_v * (p + 1), 1)
+    mu_x = mu_x_tilde[:m_x]
+    mu_v = mu_v_tilde[:m_v]
+    func_appl = func(mu_x, mu_v, params)
+    mu_x_grad, mu_v_grad = torch.autograd.functional.jacobian(lambda x, v: func(x, v, params), (mu_x, mu_v), create_graph=True)
+    mu_x_grad = _fix_grad_shape(mu_x_grad)
+    mu_v_grad = _fix_grad_shape(mu_v_grad)
+
+    # Note that this inelegant below loop can be replaced by e.g. einsum.
+    # However, an einsum implementation like this one below turns out to be
+    # slower.
+    #
+    #   mu_x_tilde_r = mu_x_tilde.reshape((p + 1, m_x))
+    #   mu_v_tilde_r = mu_v_tilde.reshape((p + 1, m_v))
+    #   func_appl_d_x = torch.einsum('kj,dj->dk', mu_x_grad, mu_x_tilde_r[1:,:]).reshape((-1, 1))
+    #   func_appl_d_v = torch.einsum('kj,dj->dk', mu_v_grad, mu_v_tilde_r[1:,:]).reshape((-1, 1))
+    #   return torch.concat((func_appl, func_appl_d_x + func_appl_d_v), dim=0)
+
+    func_appl_d = []
+    for deriv in range(1, p + 1):
+        mu_x_d = mu_x_tilde[deriv*m_x:(deriv+1)*m_x]
+        mu_v_d = mu_v_tilde[deriv*m_v:(deriv+1)*m_v]
+        func_appl_d.append(mu_x_grad @ mu_x_d + mu_v_grad @ mu_v_d)
+    return torch.vstack([func_appl] + func_appl_d)
 
 def generalized_func_batched(func, mu_x_tildes, mu_v_tildes, m_x, m_v, p, params):
     """
@@ -91,7 +131,7 @@ def generalized_func_einsum(func, mu_x_tilde, mu_v_tilde, m_x, m_v, p, params):
     func_appl_d_v = torch.einsum('kj,dj->dk', mu_v_grad, mu_v_tilde_r[1:,:]).reshape((-1, 1))
     return torch.concat((func_appl, func_appl_d_x + func_appl_d_v), dim=0)
 
-def test_correctness():
+def test_generalized_func_correctness():
     # Compares the outputs of batched generalized_func to standard
     p = 5
     x_tilde = torch.tensor(combine_gen(sin_gen(p, 0), cos_gen(p, 0)), requires_grad=True, dtype=torch.float32)
@@ -117,7 +157,7 @@ def test_correctness():
     x_tildes_b = torch.stack(x_tildes_n).clone().detach().requires_grad_()
     v_tildes_b = torch.stack(v_tildes_n).clone().detach().requires_grad_()
 
-    out_naive = [generalized_func(f, x_tilde, v_tilde, m_x, m_v, p, params)
+    out_naive = [generalized_func_naive(f, x_tilde, v_tilde, m_x, m_v, p, params)
                  for x_tilde, v_tilde in zip(x_tildes_n, v_tildes_n)]
 
     out_einsum = [generalized_func_einsum(f, x_tilde, v_tilde, m_x, m_v, p, params)
@@ -130,7 +170,7 @@ def test_correctness():
     assert torch.isclose(torch.stack(out_naive), torch.stack(out_einsum)).all()
 
 
-def test_speed():
+def test_generalized_func_speed():
     nrun = 100
     ns = [10, 100, 250, 500, 1000]
     dt = 0.1
@@ -153,7 +193,7 @@ def test_speed():
         x_tildes_b = torch.stack(x_tildes_n).clone().detach().requires_grad_()
         v_tildes_b = torch.stack(v_tildes_n).clone().detach().requires_grad_()
 
-        t_naives.append(timeit.timeit(lambda: [generalized_func(f, x_tilde, v_tilde, m_x, m_v, p, params)
+        t_naives.append(timeit.timeit(lambda: [generalized_func_naive(f, x_tilde, v_tilde, m_x, m_v, p, params)
                      for x_tilde, v_tilde in zip(x_tildes_n, v_tildes_n)],
                                       number=nrun))
         t_einsums.append(timeit.timeit(lambda: [generalized_func_einsum(f, x_tilde, v_tilde, m_x, m_v, p, params)
@@ -163,29 +203,17 @@ def test_speed():
                                         number=nrun))
         table_rows.append({
             'n': n,
-            't_naive': t_naives[-1],
-            't_einsums': t_einsums[-1],
-            't_batcheds': t_batcheds[-1]})
+            't_naive (sec per 100 runs)': t_naives[-1],
+            't_einsums (sec per 100 runs)': t_einsums[-1],
+            't_batcheds (sec per 100 runs)': t_batcheds[-1]})
     print(tabulate(table_rows, headers='keys'))
-    plt.plot(ns, t_naives, legend='Naive')
-    plt.plot(ns, t_naives, legend='Einsum')
-    plt.plot(ns, t_naives, legend='Batched')
+    plt.plot(ns, t_naives, label='Naive')
+    plt.plot(ns, t_naives, label='Einsum')
+    plt.plot(ns, t_naives, label='Batched')
     plt.legend()
     plt.show()
 
 
 if __name__ == '__main__':
-    test_correctness()
-    test_speed()
-
-    # Scribbly scribbles
-    func = f
-    mu_x = x_tildes_b[:, :m_x]
-    mu_v = v_tildes_b[:, :m_v]
-    func_appl = func(mu_x, mu_v, params)
-    mu_x_grad_b, mu_v_grad_b = torch.autograd.functional.jacobian(lambda x, v: func(x, v, params), (mu_x, mu_v), create_graph=True)
-
-    mu_x = x_tildes_b[0, :m_x]
-    mu_v = v_tildes_b[0, :m_v]
-    func_appl = func(mu_x, mu_v, params)
-    mu_x_grad_n, mu_v_grad_n = torch.autograd.functional.jacobian(lambda x, v: func(x, v, params), (mu_x, mu_v), create_graph=True)
+    test_generalized_func_correctness()
+    test_generalized_func_speed()
