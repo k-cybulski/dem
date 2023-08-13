891138d87544f7f105e942438a4887b6c2a62b24
Krzysztof Cybulski <k.cybulski.dev@tuta.io>
2023-08-14 00:42:15 +0200

    Tweak Lorenz attractor example
    Remove noise, improve hyperparameters, fix plotting

--- examples/lorenz.py
+++ examples/lorenz.py
@@ -2,6 +2,7 @@
 An example which tries to align internal state estimates of DEM with a Lorenz
 attractor. Based on an example from the dempy package.
 """
+import math
 import numpy as np
 import torch
 from scipy.integrate import solve_ivp
@@ -36,12 +37,12 @@ def g_true(x, v):
 
 rng = np.random.default_rng(215)
 # n = 1024
-n = 30
-dt = 0.5
+n = 1024 # = 500
+dt = 1
 vs = np.zeros((n, 1))
-w_sd = 1
-z_sd = 1
-noise_temporal_sig = 5
+w_sd = 0 # 1
+z_sd = 0 # 1
+noise_temporal_sig = 0.1 # practically white noise
 ts, xs, ys, ws, zs = simulate_system(f_true, g_true, x0_true, dt, vs, w_sd, z_sd, noise_temporal_sig, rng=rng)
 
 
@@ -64,7 +65,7 @@ noise_autocorr = torch.tensor(noise_cov_gen_theoretical(p, sig=noise_temporal_si
 noise_autocorr_inv_ = torch.linalg.inv(noise_autocorr)
 
 eta_theta = torch.tensor(params_true, dtype=torch.float32)
-p_theta = torch.tensor(np.diag(np.ones(eta_theta.shape)) * np.exp(5), dtype=torch.float32)
+p_theta = torch.tensor(np.diag(np.ones(eta_theta.shape)) * np.exp(4), dtype=torch.float32)
 eta_lambda = torch.zeros(2) # Not sure how to initialize these well
 p_lambda = torch.eye(2) # Not sure how to initialize these well
 
@@ -72,17 +73,6 @@ mu_x0 = torch.tensor(x0_test, dtype=torch.float32)
 mu_x0_tilde = torch.tensor(np.concatenate((x0_test, rng.normal([0] * 3 * p))), dtype=torch.float32).reshape((-1, 1))
 mu_v0_tilde = torch.tensor(np.zeros((p + 1, 1)), dtype=torch.float32)
 
-def lorenz_torch(x, v, P):
-    x = x.reshape(-1)
-    x0 = P[0] * x[1] - P[1] * x[0]
-    x1 = P[2] * x[0] - P[3] * x[2] * x[0] - P[4] * x[1]
-    x2 = P[5] * x[0] * x[1] - P[6] * x[2]
-
-    return (torch.stack([x0, x1, x2]) / 128.).reshape((-1, 1))
-
-def obs_torch(x, v, P):
-    x = x.reshape(-1)
-    return (x @ P[-3:]).reshape((-1, 1))
 
 def lorenz_torch_b(x, v, P):
     b_num = x.shape[0]
@@ -119,41 +109,46 @@ dem_input = DEMInput(dt=dt, m_x=m_x, m_v=m_v, m_y=m_y, p=p, p_comp=p_comp,
                      omega_w=torch.eye(m_x),
                      omega_z=torch.eye(m_y))
 
-# dem_state = DEMState(input=dem_input,
-#                      mu_x_tildes=None,
-#                      mu_v_tildes=None,
-#                      sig_x_tildes=[torch.eye(3 * (p + 1)) for _ in range(len_generalized(n, p_comp))],
-#                      sig_v_tildes=[torch.eye(1 * (p + 1)) for _ in range(len_generalized(n, p_comp))],
-#                      mu_theta=eta_theta,
-#                      mu_lambda=eta_lambda,
-#                      sig_theta=torch.linalg.inv(p_theta),
-#                      sig_lambda=torch.linalg.inv(p_lambda),
-#                      mu_x0_tilde=mu_x0_tilde,
-#                      mu_v0_tilde=mu_v0_tilde)
-dem_state = DEMState.from_input(dem_input, mu_x0)
+dem_state = DEMState.from_input(dem_input, mu_x0,
+                                # there will always be some discrepancy between
+                                # the model and true data due to numerical
+                                # inaccuracies. We can try to account for that
+                                # by loosening our noise.
+                                mu_lambda=torch.tensor([math.exp(-10), math.exp(-20)]))
+
+## Interlude for diagnostics
+# from hdm.core import iterate_generalized
+# x_tildes_gt = torch.stack(list(iterate_generalized(torch.tensor(xs, dtype=torch.float32), dt, p)))
+# dem_state.mu_x_tildes = x_tildes_gt
+# f_bar, extr = dem_state.free_action(diagnostic=True)
+## End interlude
 
 lr_dynamic = 1
 dem_step_d(dem_state, lr_dynamic)
-mu_xs1, sig_xs1, mu_vs1, ts1 = extract_dynamic(dem_state)
 
-plt.plot(ts, xs[:, 0], label='Target 1')
-plt.plot(ts1, mu_xs1[:, 0], label='Model 1')
-plt.plot(ts, xs[:, 1], label='Target 2')
-plt.plot(ts1, mu_xs1[:, 1], label='Model 2')
-plt.plot(ts, xs[:, 2], label='Target 3')
-plt.plot(ts1, mu_xs1[:, 2], label='Model 3')
-plt.legend()
-plt.show()
+mu_xs1, sig_xs1, mu_vs1, sig_vs1, ts1 = extract_dynamic(dem_state)
+
+fig, ax = plt.subplots()
+ax.plot(ts, xs[:, 0], label='Target 1', color='blue')
+ax.plot(ts1, mu_xs1[:, 0], label='Model 1', color='blue', linestyle='--')
+ax.plot(ts, xs[:, 1], label='Target 2', color='red')
+ax.plot(ts1, mu_xs1[:, 1], label='Model 2', color='red', linestyle='--')
+ax.plot(ts, xs[:, 2], label='Target 3', color='violet')
+ax.plot(ts1, mu_xs1[:, 2], label='Model 3', color='violet', linestyle='--')
+ax.legend()
+fig.savefig('lorenz-01.png')
+plt.close(fig)
 
 dem_step_precision(dem_state)
 dem_step_d(dem_state, lr_dynamic)
-mu_xs2, sig_xs2, mu_vs2, ts2 = extract_dynamic(dem_state)
 
-plt.plot(ts, xs[:, 0], label='Target 1')
-plt.plot(ts2, mu_xs2[:, 0], label='Model 1')
-plt.plot(ts, xs[:, 1], label='Target 2')
-plt.plot(ts2, mu_xs2[:, 1], label='Model 2')
-plt.plot(ts, xs[:, 2], label='Target 3')
-plt.plot(ts2, mu_xs2[:, 2], label='Model 3')
-plt.legend()
-plt.show()
+fig, ax = plt.subplots()
+mu_xs2, sig_xs2, mu_vs2, ts2 = extract_dynamic(dem_state)
+ax.plot(ts, xs[:, 0], label='Target 1', color='blue')
+ax.plot(ts2, mu_xs2[:, 0], label='Model 1', color='blue', linestyle='--')
+ax.plot(ts, xs[:, 1], label='Target 2', color='red')
+ax.plot(ts2, mu_xs2[:, 1], label='Model 2', color='red', linestyle='--')
+ax.plot(ts, xs[:, 2], label='Target 3', color='violet')
+ax.plot(ts2, mu_xs2[:, 2], label='Model 3', color='violet', linestyle='--')
+ax.legend()
+fig.savefig('lorenz-02.png')
