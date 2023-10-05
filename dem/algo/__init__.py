30219e8363d50084d0598b1bbd7c41401b233392
Krzysztof Cybulski <k.cybulski.dev@tuta.io>
2023-10-05 12:15:35 +0200

    Make JAX the default implementation
    Now `from dem.algo import DEMInput, DEMState` imports the JAX
    implementation

--- dem/algo/__init__.py
+++ dem/algo/__init__.py
@@ -1,4 +1,11 @@
 """
 This module contains implementations of DEM. These implementations differ in
-how optimized they are.
+how optimized they are. The default and most effective by far implementation is
+in JAX.
 """
+
+from .jax.algo import DEMInputJAX, DEMStateJAX, extract_dynamic
+
+# JAX implementation is the default
+DEMInput = DEMInputJAX
+DEMState = DEMStateJAX
